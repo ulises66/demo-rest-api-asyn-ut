fd3b3e8080b39a7b905e73a2a797972ff03c7055
Ulises_Aguilar <uaguilar@healthcare.com>
2021-02-11 00:34:26 -0600

    style(core): Update rules of commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -14,4 +14,10 @@
 // controllers
 const scope = ['core', 'search']
 
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [2, 'always', Infinity],
+        'scope-enum': [2, 'always', scope],
+        'subject-case': [2, 'always', 'sentence-case'],
+        'references-empty': [2, 'never']
+    }}
